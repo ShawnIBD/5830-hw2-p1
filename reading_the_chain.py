840cab8ae711895e672c1cbd33adee646debc48f
Shawn Zhang <zhansha@seas.upenn.edu>
2026-07-04 12:43:55 +0000

    fix homework 3

--- reading_the_chain.py
+++ reading_the_chain.py
@@ -37,18 +37,18 @@ def connect_with_middleware(contract_json):
   return w3, contract
 
 def is_ordered_block(w3, block_num):
-	"""
-	Takes a block number
-	Returns a boolean that tells whether all the transactions in the block are ordered by priority fee
+    """
+    Takes a block number
+    Returns a boolean that tells whether all the transactions in the block are ordered by priority fee
 
-	Before EIP-1559, a block is ordered if and only if all transactions are sorted in decreasing order of the gasPrice field
+    Before EIP-1559, a block is ordered if and only if all transactions are sorted in decreasing order of the gasPrice field
 
-	After EIP-1559, there are two types of transactions
-		*Type 0* The priority fee is tx.gasPrice - block.baseFeePerGas
-		*Type 2* The priority fee is min( tx.maxPriorityFeePerGas, tx.maxFeePerGas - block.baseFeePerGas )
+    After EIP-1559, there are two types of transactions
+        *Type 0* The priority fee is tx.gasPrice - block.baseFeePerGas
+        *Type 2* The priority fee is min( tx.maxPriorityFeePerGas, tx.maxFeePerGas - block.baseFeePerGas )
 
-	Conveniently, most type 2 transactions set the gasPrice field to be min( tx.maxPriorityFeePerGas + block.baseFeePerGas, tx.maxFeePerGas )
-	"""
+    Conveniently, most type 2 transactions set the gasPrice field to be min( tx.maxPriorityFeePerGas + block.baseFeePerGas, tx.maxFeePerGas )
+    """
   block = w3.eth.get_block(block_num, full_transactions=True)
   ordered = False
 
@@ -96,22 +96,22 @@ def is_ordered_block(w3, block_num):
 
 
 def get_contract_values(contract, admin_address, owner_address):
-	"""
-	Takes a contract object, and two addresses (as strings) to be used for calling
-	the contract to check current on chain values.
-	The provided "default_admin_role" is the correctly formatted solidity default
-	admin value to use when checking with the contract
-	To complete this method you need to make three calls to the contract to get:
-	  onchain_root: Get and return the merkleRoot from the provided contract
-	  has_role: Verify that the address "admin_address" has the role "default_admin_role" return True/False
-	  prime: Call the contract to get and return the prime owned by "owner_address"
+    """
+    Takes a contract object, and two addresses (as strings) to be used for calling
+    the contract to check current on chain values.
+    The provided "default_admin_role" is the correctly formatted solidity default
+    admin value to use when checking with the contract
+    To complete this method you need to make three calls to the contract to get:
+      onchain_root: Get and return the merkleRoot from the provided contract
+      has_role: Verify that the address "admin_address" has the role "default_admin_role" return True/False
+      prime: Call the contract to get and return the prime owned by "owner_address"
 
-	check on available contract functions and transactions on the block explorer at
-	https://testnet.bscscan.com/address/0xaA7CAaDA823300D18D3c43f65569a47e78220073
-	"""
+    check on available contract functions and transactions on the block explorer at
+    https://testnet.bscscan.com/address/0xaA7CAaDA823300D18D3c43f65569a47e78220073
+    """
   default_admin_role = int.to_bytes(0, 32, byteorder="big")
 
-	# TODO complete the following lines by performing contract calls
+    # TODO complete the following lines by performing contract calls
 
   # Get and return the merkleRoot from the provided contract
   onchain_root = contract.functions.merkleRoot().call()
@@ -122,36 +122,36 @@ def get_contract_values(contract, admin_address, owner_address):
     admin_address
   ).call()
 
-	# Call the contract to get the prime owned by "owner_address"
+    # Call the contract to get the prime owned by "owner_address"
   prime = contract.functions.getPrimeByOwner(owner_address).call()
 
   return onchain_root, has_role, prime
 
 
 """
-	This might be useful for testing (main is not run by the grader feel free to change 
-	this code anyway that is helpful)
+    This might be useful for testing (main is not run by the grader feel free to change 
+    this code anyway that is helpful)
 """
 if __name__ == "__main__":
-	# These are addresses associated with the Merkle contract (check on contract
-	# functions and transactions on the block explorer at
-	# https://testnet.bscscan.com/address/0xaA7CAaDA823300D18D3c43f65569a47e78220073
-	admin_address = "0xAC55e7d73A792fE1A9e051BDF4A010c33962809A"
-	owner_address = "0x793A37a85964D96ACD6368777c7C7050F05b11dE"
-	contract_file = "contract_info.json"
+    # These are addresses associated with the Merkle contract (check on contract
+    # functions and transactions on the block explorer at
+    # https://testnet.bscscan.com/address/0xaA7CAaDA823300D18D3c43f65569a47e78220073
+    admin_address = "0xAC55e7d73A792fE1A9e051BDF4A010c33962809A"
+    owner_address = "0x793A37a85964D96ACD6368777c7C7050F05b11dE"
+    contract_file = "contract_info.json"
 
-	eth_w3 = connect_to_eth()
-	cont_w3, contract = connect_with_middleware(contract_file)
+    eth_w3 = connect_to_eth()
+    cont_w3, contract = connect_with_middleware(contract_file)
 
-	latest_block = eth_w3.eth.get_block_number()
-	london_hard_fork_block_num = 12965000
-	assert latest_block > london_hard_fork_block_num, f"Error: the chain never got past the London Hard Fork"
+    latest_block = eth_w3.eth.get_block_number()
+    london_hard_fork_block_num = 12965000
+    assert latest_block > london_hard_fork_block_num, f"Error: the chain never got past the London Hard Fork"
 
-	n = 5
-	for _ in range(n):
-		block_num = random.randint(1, latest_block)
-		ordered = is_ordered_block(block_num)
-		if ordered:
-			print(f"Block {block_num} is ordered")
-		else:
-			print(f"Block {block_num} is not ordered")
+    n = 5
+    for _ in range(n):
+        block_num = random.randint(1, latest_block)
+        ordered = is_ordered_block(block_num)
+        if ordered:
+            print(f"Block {block_num} is ordered")
+        else:
+            print(f"Block {block_num} is not ordered")
